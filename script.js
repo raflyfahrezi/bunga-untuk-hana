728dd9d580ca104b0c6d8afde138dac6680c8e64
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2024-08-08 20:21:00 +0700

    update

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+import './scripts/components/slider.js'
+import sliderScript from './scripts/slider.js'
+
 document.addEventListener('DOMContentLoaded', () => {
+  sliderScript()
+
   const c = setTimeout(() => {
     document.body.classList.remove('not-loaded')
     clearTimeout(c)
