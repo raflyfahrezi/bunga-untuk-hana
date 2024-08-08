e34d30223709008ec88f6f7c5f34495dbbb7bbcd
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2024-08-08 21:24:20 +0700

    update

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ import sliderScript from './scripts/slider.js'
 document.addEventListener('DOMContentLoaded', () => {
   sliderScript()
 
+  const backsoundAudio = document.getElementById('backsoundAudio')
+  const buttonplay = document.getElementById('buttonplay')
+
+  buttonplay.addEventListener('click', () => {
+    backsoundAudio.play()
+  })
+
   const c = setTimeout(() => {
     document.body.classList.remove('not-loaded')
     clearTimeout(c)
