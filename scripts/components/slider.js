6c66b3d2449d7fb7cd043681c59a9b42b4d91f32
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2024-08-08 20:37:12 +0700

    update

--- scripts/components/slider.js
+++ scripts/components/slider.js
@@ -12,15 +12,15 @@ class Carousel extends HTMLElement {
         <div style="width: 100%; position: absolute; top: 40%; left: 50%; transform: translate(-50%, -50%); z-index: 20;" class="text-center">
           <div class="wrapper">
             <ul class="carousel">
-                <li class="card" style="background-image: url('../assets/image-1.jpg');"></li>
-                <li class="card" style="background-image: url('../assets/image-3.jpg');"></li>
-                <li class="card" style="background-image: url('../assets/image-2.jpg');"></li>
-                <li class="card" style="background-image: url('../assets/image-4.jpg');"></li>
-                <li class="card" style="background-image: url('../assets/image-8.jpg');"></li>
-                <li class="card" style="background-image: url('../assets/image-5.jpg');"></li>
-                <li class="card" style="background-image: url('../assets/image-6.jpg');"></li>
-                <li class="card" style="background-image: url('../assets/image-7.jpg');"></li>
-                <li class="card" style="background-image: url('../assets/image-9.jpg');"></li>
+                <li class="card" style="background-image: url('/assets/image-1.jpg');"></li>
+                <li class="card" style="background-image: url('/assets/image-3.jpg');"></li>
+                <li class="card" style="background-image: url('/assets/image-2.jpg');"></li>
+                <li class="card" style="background-image: url('/assets/image-4.jpg');"></li>
+                <li class="card" style="background-image: url('/assets/image-8.jpg');"></li>
+                <li class="card" style="background-image: url('/assets/image-5.jpg');"></li>
+                <li class="card" style="background-image: url('/assets/image-6.jpg');"></li>
+                <li class="card" style="background-image: url('/assets/image-7.jpg');"></li>
+                <li class="card" style="background-image: url('/assets/image-9.jpg');"></li>
             </ul>
           </div>
         </div>
