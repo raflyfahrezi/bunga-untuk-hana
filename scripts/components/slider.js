e2076cb562fec1a219066b9c5a882c112c3dfc0e
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2024-08-08 20:52:04 +0700

    update

--- scripts/components/slider.js
+++ scripts/components/slider.js
@@ -9,7 +9,21 @@ class Carousel extends HTMLElement {
 
   render(h) {
     this.innerHTML = `
-       
+        <div style="width: 100%; position: absolute; top: 40%; left: 50%; transform: translate(-50%, -50%); z-index: 20;" class="text-center">
+          <div class="wrapper">
+            <ul class="carousel">
+                <li class="card" style="background-image: url('../bunga-untuk-hana/assets/image-1.jpg');"></li>
+                <li class="card" style="background-image: url('../bunga-untuk-hana/assets/image-3.jpg');"></li>
+                <li class="card" style="background-image: url('../bunga-untuk-hana/assets/image-2.jpg');"></li>
+                <li class="card" style="background-image: url('../bunga-untuk-hana/assets/image-4.jpg');"></li>
+                <li class="card" style="background-image: url('../bunga-untuk-hana/assets/image-8.jpg');"></li>
+                <li class="card" style="background-image: url('../bunga-untuk-hana/assets/image-5.jpg');"></li>
+                <li class="card" style="background-image: url('../bunga-untuk-hana/assets/image-6.jpg');"></li>
+                <li class="card" style="background-image: url('../bunga-untuk-hana/assets/image-7.jpg');"></li>
+                <li class="card" style="background-image: url('../bunga-untuk-hana/assets/image-9.jpg');"></li>
+            </ul>
+          </div>
+        </div>
     `
   }
 }
